dbcf2338bd8973ca1e8ea3ab691a7a00c4fca8f6
NoSequel <nosequel@protonmail.com>
2021-01-26 06:50:02 +0000

    improve structure and add compile.sh script

---
+++ compile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+g++ ./src/main.c
+
+if [ "$EUID" -ne 0 ] 
+then
+    echo "You must run this command as root"
+else
+    # if the command is ran by root, install under /bin/efetch
+    echo "Installing under /bin/efetch"
+
+    # delete /bin/efetch if it exists
+    if [ -f "/bin/efetch" ] 
+    then
+        echo "Deleting old /bin/efetch file"
+        rm -rf /bin/efetch
+    fi
+
+    echo "Making new symlink for /bin/efetch"
+    # make a new symlink for efetch
+    ln ./a.out /bin/efetch
+    echo "Finished."
+fi
