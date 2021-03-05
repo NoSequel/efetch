eac1aa0d10e2d97113099942f025aef9af651eb1
NoSequel <nosequel@protonmail.com>
2021-03-05 05:02:35 +0000

    add support for more colors

--- src/color.h
+++ src/color.h
@@ -2,13 +2,25 @@
 
 namespace Color {
     enum Code {
+        // FOREGROUND COLORS
+        FG_BLACK    = 30,
         FG_RED      = 31,
         FG_GREEN    = 32,
+        FG_YELLOW   = 33,
         FG_BLUE     = 34,
+        FG_MAGENTA  = 35,
+        FG_CYAN     = 36,
+        FG_WHITE    = 37,
         FG_DEFAULT  = 39,
+
+        // BACKGROUND COLORS
+        BG_BLACK    = 40,
         BG_RED      = 41,
         BG_GREEN    = 42,
+        BG_YELLOW   = 43,
         BG_BLUE     = 44,
+        BG_MAGENTA  = 45,
+        BG_CYAN     = 46,
         BG_DEFAULT  = 49
     };
     class Modifier {
--- src/config.h
+++ src/config.h
@@ -5,20 +5,11 @@ static const Color::Modifier primaryColor(Color::FG_DEFAULT);
 static const Color::Modifier secondaryColor(Color::FG_BLUE);
 
 // toggleable fields
-// the hostname is retrieved using $HOSTNAME
 static const bool displayHostname(true);
-
-// the kernel version is retrieved using 'uname -r'
 static const bool displayKernelVersion(true);
-
-// the operating system is checked using the `/etc/relases` file
 static const bool displayOperatingSystem(true);
-
-// the shell is retrieved using $SHELL
 static const bool displayShell(true);
-
-// the amount of packages is OS-specific
-static const bool displayPackages (true);
+static const bool displayPackages(true);
 
 // fallback variables if no OS was found
 static const char* osName("Gentoo/Linux");
